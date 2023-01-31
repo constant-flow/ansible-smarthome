6e51962e7633e81a0cc988a9a716d550167318e1
Constantin Wolf <constantin.wolf@pwc.com>
2023-01-31 23:22:49 +0100

    host mismatch warning

--- run-dev.sh
+++ run-dev.sh
@@ -4,4 +4,4 @@
 export ANSIBLE_FORCE_COLOR=True
 
 # pull and run the playbook
-ansible-pull -U https://github.com/constant-flow/ansible-smarthome -i hosts playbook-automation.yml
+ansible-pull -U https://github.com/constant-flow/ansible-smarthome -i localhost playbook-automation.yml
